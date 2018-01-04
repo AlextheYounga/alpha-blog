7c30e22d1136560159acf22313c976f0e22ad9f1
alexyounger <alexyounger07@gmail.com>
2018-01-04 22:54:05 +0000

    Added a destroy method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,24 @@
 class ArticlesController < ApplicationController #class name has to be CamelCase
  
-#THIS IS THE CONTROLLER FOR THE INDEX, OR THE LISTING ARTICLES PAGE  
+#THIS IS THE METHOD FOR THE INDEX, OR THE LISTING ARTICLES PAGE  
 def index
   @articles = Article.all
 end
 
 
-# THIS IS THE CONTROLLER FOR THE NEW PAGE
+# THIS IS THE METHOD FOR THE NEW PAGE
   def new   #it's called "new" because it's a new articles page
     @article = Article.new
   end
   
  
-#THIS IS THE CONTROLLER FOR THE EDIT PAGE
+#THIS IS THE METHOD FOR THE EDIT PAGE
 def edit
   @article = Article.find(params[:id])
 end
   
 
-#THIS IS THE CONTROLLER FOR THE ARTICLE CREATION METHOD  
+#THIS IS THE METHOD FOR THE ARTICLE CREATION METHOD  
 def create #this is for the process of sending an article to the database
    
     @article =Article.new(article_params) #This is the instance variable that will allow your article to be saved from the create method
@@ -32,7 +32,7 @@ else
 end
 
 
-#THIS IS THE CONTROLLER FOR THE ARTICLE UPDATE METHOD
+#THIS IS THE METHOD FOR THE ARTICLE UPDATE METHOD
 def update  
   @article = Article.find(params[:id])
   if @article.update(article_params)
@@ -44,13 +44,22 @@ def update
 end
 
 
-#THIS IS THE CONTROLLER FOR THE SHOW PAGE 
+#THIS IS THE METHOD FOR THE SHOW PAGE 
 def show
     @article = Article.find(params[:id])   #article id is going to be passed through the params hash
 end
   
+
+#THIS IS THE METHOD FOR THE DESTROY FUNCTION
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  flash[:notice] = "Article was successfully deleted"
+  redirect_to articles_path
+end
   
-#THIS IS THE CONTROLLER FOR THE ARTICLE PARAMETERS FUNCTION
+  
+#THIS IS THE METHOD FOR THE ARTICLE PARAMETERS FUNCTION
  private
   def article_params
     params.require(:article).permit(:title, :description)   #The params hash and the permissions will save the article title and description 
