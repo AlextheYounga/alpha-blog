39d4f4e41454ef31f2602a18aed592784fd2e5a0
alexyounger <alexyounger07@gmail.com>
2018-01-18 22:33:15 +0000

    Fixed user bug, style messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password_digest)
+    params.require(:user).permit(:username, :email, :password)
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ActiveRecord::Base
   has_many :articles
   before_save { self.email = email.downcase }
-  validates :username, presence: true, uniqueness: {case_sensative: false },
+  validates :username, presence: true, uniqueness: {case_sensitive: false },
              length: {minimum: 3, maximum: 25}
              
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :email, presence: true, length: {maximum: 105}, 
-            uniqueness: {case_sensative: false}, 
+            uniqueness: {case_sensitive: false}, 
             format: {with: VALID_EMAIL_REGEX }
   
   has_secure_password
