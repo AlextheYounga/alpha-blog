4ce24beaacf45f6376945ad56adfcfc7e6b4a51c
alexyounger <alexyounger07@gmail.com>
2018-05-19 18:03:38 +0000

    Configure incorrect search alerts for stock-lookup

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,13 +3,19 @@ require 'stock_quote'
 class StocksController < ApplicationController
   
   def search
-    if params[:symbol].present?
+    if params[:stock].present?
       @stock = Stock.new_from_lookup(params[:stock])
-      render 'pages/playground'
+        if @stock
+          render 'pages/playground'
+        else
+          flash[:danger] = "You have entered an incorrect symbol"
+          redirect_to playground_path
+        end
       
-    else
-      flash[:danger] = "You have entered an empty search string"
-      redirect_to playground_path
+        else
+          flash[:danger] = "You have entered an empty search string"
+          redirect_to playground_path
+        
     end
   end
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,11 +1,15 @@
 class Stock < ActiveRecord::Base
   
   def self.new_from_lookup(symbol)
-    stock = StockQuote::Stock.quote(symbol)
-    new(company_name: stock.company_name,
-        symbol: stock.symbol,
-        latest_price: stock.latest_price,
-        latest_time: stock.latest_time)
+    begin
+      stock = StockQuote::Stock.quote(symbol)
+      new(company_name: stock.company_name,
+          symbol: stock.symbol,
+          latest_price: stock.latest_price,
+          latest_time: stock.latest_time)
+    rescue Exception => e
+      return nil
+    end
   end
   
 end
