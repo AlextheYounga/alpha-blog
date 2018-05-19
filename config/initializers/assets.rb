1392a3105c4cfcf344485b6aefed4ad93b592c96
alexyounger <alexyounger07@gmail.com>
2018-05-19 06:51:19 +0000

    Fixed Styling Bug

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( carousel.js )
 Rails.application.config.assets.precompile += %w( readinglist.js )
 Rails.application.config.assets.precompile += %w( quote_generator.js )
+Rails.application.config.assets.precompile += %w( timeline.js )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
