fa5b6e4cb3f749648a4e34d6fc1d341a87781b20
alexyounger <alexyounger07@gmail.com>
2018-05-19 17:55:17 +0000

    stock-lookup styling update 1.1

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,8 +3,13 @@ require 'stock_quote'
 class StocksController < ApplicationController
   
   def search
-   @stock = Stock.new_from_lookup(params[:stock])
-   render 'pages/playground'
+    if params[:symbol].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+      render 'pages/playground'
+      
+    else
+      flash[:danger] = "You have entered an empty search string"
+      redirect_to playground_path
+    end
   end
-  
 end
