66facf1b7ef517fdf82961b8b491fa7714f80dfe
alexyounger <alexyounger07@gmail.com>
2018-06-05 04:02:56 +0000

    deleted old migration files

--- db/migrate/20180517202216_add_timetables_to_stock.rb
+++
@@ -1,5 +0,0 @@
-class AddTimetablesToStock < ActiveRecord::Migration
-  def change
-    add_column :stocks, :latest_time, :datetime
-  end
-end
--- db/migrate/20180517205823_add_updatedtime_to_stocks.rb
+++
@@ -1,5 +0,0 @@
-class AddUpdatedtimeToStocks < ActiveRecord::Migration
-  def change
-    add_column :stocks, :latest_update, :datetime
-  end
-end
